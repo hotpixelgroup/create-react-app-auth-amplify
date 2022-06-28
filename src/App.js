840fbffe93a645b037fd6a50f6a68e587be6b70f
Harry Beckwith <harry@hotpixelgroup.com>
2022-06-28 12:16:16 -0700

    Revert "testing hide sign up"
    This reverts commit 4750122a8314feafb48a02cf03a8200ee4cb0202.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { AmplifyAuthenticator } from '@aws-amplify/ui-react'
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
@@ -8,18 +9,25 @@ Amplify.configure(aws_exports);
 class App extends Component {
   render() {
     return (
-        <AmplifyAuthenticator
-            hideSignUp={true}
-        >
-            {({ signOut, user }) => (
-                <main>
-                    <h1>Hello {user.username}</h1>
-                    <button onClick={signOut}>Sign out</button>
-                </main>
-            )}
-        </AmplifyAuthenticator>
+      <div className="App">
+        <AmplifySignOut />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
     );
   }
 }
 
-export default App;
+export default withAuthenticator(App);
